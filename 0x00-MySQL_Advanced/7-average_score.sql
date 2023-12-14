2555413d1d248f6e4ca9c8e867741ac11ee40384
Sue609 <susankamau609@gmail.com>
2023-12-14 17:09:21 +0000

    SQL script that creates a stored procedure ComputeAverageScoreForUser

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,5 @@
---sql script that creates a stored procedure
-
+-- sql script that creates a stored procedure that computes
+-- and stores the average score for a student
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
