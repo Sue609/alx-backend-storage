70557d382d396f326e60515813ab88a71d4d8c20
Sue609 <susankamau609@gmail.com>
2023-12-20 11:52:33 +0000

    incrementing values using redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,25 @@ module that writes strings to redis
 """
 import redis
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator function for coounting how many times
+    the methods of the class are called.
+    """
+
+    @wraps(method)
+    def wrappers(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        key = method.__qualname__
+        self.redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -27,29 +45,34 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, None]:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """
         Reading from redis and recovering original type.
         """
-        value = self._redis.get(Key)
-        if value is None:
-            return None
+        value = self._redis.get(key)
         if fn:
             return fn(value)
         return value
 
-    def get_str(self, key: str) -> Union[str, None]:
+    def get_str(self, key: str) -> str:
         """
         uses the get method with a lambda function to decode
         the retrieved data as UTF-8 and returns it as a string or
         None if the key does not exist.
         """
-        return self.get(key, fn=lambda d: d.decode('utf-8'))
+        value = self._redis.get(key)
+        return value.decode('utf-8')
 
-    def get_int(self, key: str) -> Union[int, None]:
+    def get_int(self, key: str) -> int:
         """
         uses the get method with a lambda function to convert the
         retrieved data to an integer or None if the key does not exist.
         """
-        return self.get(key, fn=lambda d: int(d))
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
 
